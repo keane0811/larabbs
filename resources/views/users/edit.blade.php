7e2da213b9146540b366d1038bbc32c5a9c94ae6
Keane <jinxin0811@hotmail.com>
2021-04-02 04:20:33 +0800

    upload avatar

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -11,7 +11,8 @@
         </div>
 
         <div class="card-body">
-          <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8">
+          <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8"
+            enctype="multipart/form-data">
             @method('PUT')
             @csrf
 
@@ -22,16 +23,28 @@
               <input type="text" class="form-control" name="name" id="name-field"
                 value="{{ old('name', $user->name) }}">
             </div>
+
             <div class="form-group">
               <label for="email-field">邮 箱</label>
               <input type="text" class="form-control" name="email" id="email-field"
                 value="{{ old('email', $user->email) }}">
             </div>
+
             <div class="form-group">
               <label for="introduction-field">个人简介</label>
               <textarea name="introduction" id="introduction-field" rows="3"
                 class="form-control">{{ old('introduction', $user->introduction) }}</textarea>
             </div>
+
+            <div class="form-group mb-4">
+              <label for="" class="avatar-label">用户头像</label>
+              <input type="file" class="form-control-file" name="avatar">
+              @if ($user->avatar)
+                <br>
+                <img src="{{ $user->avatar }}" class="thumbnail img-responsive" width="200">
+              @endif
+            </div>
+
             <div class="well well-sm">
               <button class="btn btn-primary" type="submit">保存</button>
             </div>
