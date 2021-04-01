2b999d61480fe2be06c3379c5dc688ae6cd4abb7
Keane <jinxin0811@hotmail.com>
2021-04-02 01:42:31 +0800

    编辑个人资料

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
 
-Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
