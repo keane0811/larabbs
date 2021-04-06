bade3752a5a300bbcd7020e42b35a63692e6646f
Keane <jinxin0811@hotmail.com>
2021-04-07 05:30:35 +0800

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Auth::routes(['verify' => true]);
 Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => 'show']);
