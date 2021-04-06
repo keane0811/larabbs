500e09735b9275b50b593694bb5bc0ac72816491
Keane <jinxin0811@hotmail.com>
2021-04-06 23:22:26 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
