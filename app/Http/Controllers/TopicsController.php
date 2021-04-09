7bbd7d6880fc6716b9f38df4ea0f1b71f4c5ab9a
Keane <jinxin0811@hotmail.com>
2021-04-10 01:43:07 +0800

    用户可以上传话题图片

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -62,5 +63,29 @@ class TopicsController extends Controller
         $topic->delete();
 
         return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+    }
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据, 默认是失败
+        $data = [
+            'success' => false,
+            'msg' => '上传失败',
+            'file_path' => '',
+        ];
+        // 判断是否有上传文件, 并赋值给 $file
+        if ($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($file, 'topics', Auth::id(), 1024);
+            // 图片保存成功的话
+            if ($result) {
+                $data = [
+                    'success' => true,
+                    'msg' => '上传成功',
+                    'file_path' => $result['path'],
+                ];
+            }
+        }
+        return $data;
     }
 }
