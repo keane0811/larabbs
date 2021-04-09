d5edfa262c41f635822f1eae8b16095a26446d04
Keane <jinxin0811@hotmail.com>
2021-04-10 02:16:09 +0800

    topic show page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>@yield('title', 'LaraBBS')</title>
+  <meta name="description" content="@yield('description', 'LaraBBS')">
 
   {{-- Styles --}}
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
