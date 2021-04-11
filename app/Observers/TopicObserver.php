b8d7188774d08207efe0d03356cb40e1225c1957
Keane <jinxin0811@hotmail.com>
2021-04-12 02:44:42 +0800

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,7 +2,7 @@
 
 namespace App\Observers;
 
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -26,9 +26,14 @@ class TopicObserver
         $topic->body = clean($topic->body, 'user_topic_body');
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+    }
+
+    public function saved(Topic $topic)
+    {
         // 如果 slug 字段无内容, 就使用翻译器对 title 进行翻译
         if (!$topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
