e28c45a9ba4868190c812efce97fdb7f55be5b5a
Keane <jinxin0811@hotmail.com>
2021-04-11 17:06:12 +0800

    翻译 url 标识

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ Auth::routes(['verify' => true]);
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => 'show']);
 
