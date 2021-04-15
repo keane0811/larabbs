ba59a83e2fd7c12ac0414cb48510912bd3e8488f
Keane <jinxin0811@hotmail.com>
2021-04-15 14:33:57 +0800

    管理后台权限

--- config/administrator/permissions.php
+++ config/administrator/permissions.php
@@ -1,15 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Auth;
-use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 return [
-    'title' => '角色',
-    'single' => '角色',
-    'model' => Role::class,
+    'title' => '权限',
+    'single' => '权限',
+    'model' => Permission::class,
     'permission' => function () {
-        return Auth::user()->can('manage_contents');
+        return Auth::user()->can('manage_users');
     },
+    // 对crud动作的单独权限控制, 通过返回布尔值来控制权限
+    'action_permissions' => [
+        // 控制新建按钮的显示
+        'create' => function ($model) {
+            return true;
+        },
+        // 允许更新
+        'update' => function ($model) {
+            return true;
+        },
+        // 不允许删除
+        'delete' => function ($model) {
+            return false;
+        },
+        // 允许查看
+        'view' => function ($model) {
+            return true;
+        },
+    ],
     'columns' => [
         'id' => [
             'title' => 'ID',
@@ -17,51 +36,26 @@ return [
         'name' => [
             'title' => '标识'
         ],
-        'permissions' => [
-            'title' => '权限',
-            'output' => function ($value, $model) {
-                $model->load('permissions');
-                $result = [];
-                foreach ($model->permissions as $permission) {
-                    $result[] = $permission->name;
-                }
-                return empty($result) ? 'N/A' : implode(' | ', $result);
-            },
-            'sortable' => false,
-        ],
         'operation' => [
             'title' => '管理',
-            'output' => function ($value, $model) {
-                return $value;
-            },
             'sortable' => false,
         ],
     ],
     'edit_fields' => [
         'name' => [
-            'title' => '标识',
+            'title' => '标识(请慎重修改)',
+            // 表单条目标题旁的提示信息
+            'hint' => '修改权限标识会影响代码的调用, 请不要轻易更改',
         ],
-        'permissions' => [
-            'title' => '权限',
+        'roles' => [
+            'title' => '角色',
             'type' => 'relationship',
             'name_field' => 'name',
         ],
     ],
     'filters' => [
-        'id' => [
-            'title' => 'ID',
-        ],
         'name' => [
             'title' => '标识',
         ],
-    ],
-    // 新建和编辑时的表单验证规则
-    'rules' => [
-        'name' => 'required|max:15|unique:roles,name',
-    ],
-    // 表单验证错误时定制错误消息
-    'messages' => [
-        'name.required' => '标识不能为空',
-        'name.unique' => '标识已存在',
     ],
 ];
